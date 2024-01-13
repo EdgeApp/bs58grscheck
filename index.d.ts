9bfcc25292ba4904d4d6f2ef05c783b96cda5b40
HashEngineering <hashengineeringsolutions@gmail.com>
2024-01-13 07:32:22 -0800

    use grs noble-hashes

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="node" />
 
-declare const bs58check: {
+declare const bs58grscheck: {
     encode(buffer: Buffer | number[] | Uint8Array): string;
     decodeUnsafe(string: string): Uint8Array | undefined;
     decode(string: string): Uint8Array;
 };
 
-export = bs58check;
+export = bs58grscheck;
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 'use strict'
 
-var { sha256 } = require('@noble/hashes/sha256')
+var { groestl256 } = require('@noble/hashes/groestl256')
+var { groestl512 } = require('@noble/hashes/groestl512')
 var bs58grscheckBase = require('./base')
-
-// SHA256(SHA256(buffer))
-function sha256x2 (buffer) {
-  return sha256(sha256(buffer))
-}
 
 // GROESTL512(GROESTL512(buffer))
 function groestl (buffer) {
-  return Buffer(groestlhash.groestl_2(buffer, 1, 1))
+  return Buffer.from(groestl256(groestl512(buffer)))
 }
 module.exports = bs58grscheckBase(groestl)
